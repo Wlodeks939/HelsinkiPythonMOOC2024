46c264f62cb3820851e31ce61a34b5e5266428fc
Wlodeks939 <wsompolinski@wp.pl>
2024-06-22 17:09:36 +0200

    Zmiana 01_1

--- Part04/01_hello_visual_studio_code.py
+++ Part04/01_hello_visual_studio_code.py
@@ -1,12 +1,4 @@
 # Write your solution here
-
-while True:
-    string = input("Editor: ")
-    
-    if "visual studio code" == string.lower():
-        print("an excellent choice!")
-        break
-    if "word" == string.lower():
-        print("awful")
-    else:
-        print("not good")
+editori = input("editori: ")  
+    if editori == "word" or editori == "notepad":
+        print("surkea")
