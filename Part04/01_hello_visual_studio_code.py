dffb8b3d8ffccbd673c7ed86f22483a0a074e35b
Wlodeks939 <wsompolinski@wp.pl>
2024-06-22 18:04:42 +0200

    01_02G

--- Part04/01_hello_visual_studio_code.py
+++ Part04/01_hello_visual_studio_code.py
@@ -1,4 +1,7 @@
 # Write your solution here
+mystring = "Visual Studio CODE"
+if "visual studio code" = mystring lower():
+    print("this was the string I was looking for!")
 editori = input("editori: ")  
 if editori == "word" or editori == "notepad":
     print("surkea")
